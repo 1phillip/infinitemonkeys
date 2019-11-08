6f9cd33d40d5f4006af30fee50cd6518b1981e65
1phillip <50423954+1phillip@users.noreply.github.com>
2019-11-08 15:16:40 -0800

    Update monkeys.py
    Changed code so that it produces a constant stream of random characters instead of repeatedly generating a random string. Will check for a match within the last X characters of the constant stream where X is specified by the user.

--- monkeys.py
+++ monkeys.py
@@ -1,32 +1,27 @@
 import random, string
-#Open the passage.txt and store the entire text in the passage variable
+#Open the passage.txt and store the entire text in the "passage" variable
 f = open("passage.txt", "r")
 passage = f.read()
 
-#Obtain list of all possible characters in the passage
+#Obtain list of all possible characters in the passage and store it in the "allCharacters" variable
 allCharacters = []
 for character in passage:
     if character not in allCharacters:
         allCharacters += character
-
-#Generate random string
-def randomString(stringLength): 
-    return ''.join(random.choice(allCharacters) for i in range(stringLength))
-
-#Prompt user for string length
-stringLength = input("Length of randomly generated search query: ")
+        
+total = ""
+guess = ""
+stringLength = input("Desired length of search query: ")
 
 #Begin generating random strings
-attemptNumber = 1
-guess = ""
 while True:
-    print("Attempt " + str(attemptNumber) + ": ", end = "")
-    guess = randomString(int(stringLength))
-    print(guess)
-    attemptNumber += 1
+    temp = random.choice(allCharacters)
+    total += temp
+    print(temp, end = '')
+    guess = total[-int(stringLength):]
 
     #When string is found in passage
-    if guess in passage:
+    if guess in passage and len(guess) >= int(stringLength):
         break
-print("\nThe string " + '"' + guess + '"' + " was found on attempt " + str(attemptNumber) + " in the following location:\n")
-print("..." + passage[passage.find(guess)-100:passage.find(guess)+100] + "...") #Print the section of passage where the random string was found
+print("\n\nThe string " + '"' + guess + '"' + " was found in the following location:\n")
+print("..." + passage[passage.find(guess)-100:passage.find(guess)+100] + "...") #Print the section of passage where the random string was found.
